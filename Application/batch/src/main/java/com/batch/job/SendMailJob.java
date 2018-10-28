bf66bcac3ee45e5f4a2a4bc282af5c57a95db991
tomneozed <tomneozed@gmail.com>
2018-10-28 18:54:56 +0100

    implementing mail sender and scheduler

--- Application/batch/src/main/java/com/batch/job/SendMailJob.java
+++ Application/batch/src/main/java/com/batch/job/SendMailJob.java
@@ -1,41 +1,23 @@
 package com.batch.job;
 
+import com.batch.SendMail;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 
-public class SendMailJob implements Job {
-	@Autowired
-	private JavaMailSender javaMailSender;
+public class SendMailJob implements Job
+{
+	SendMail sendMail = new SendMail();
 
 
-	public SendMailJob(JavaMailSender javaMailSender)
+	public SendMailJob()
 	{
-		this.javaMailSender = javaMailSender;
+
 	}
 
-	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+	public void execute(JobExecutionContext arg0){
 
-		System.out.println("HelloWorld");
-
-		SimpleMailMessage mail = new SimpleMailMessage();
-
-
-		mail.setTo("pelissier.thomas31@gmail.com");
-		mail.setFrom("pelissier.thomas31@gmail.com");
-		mail.setSubject("Bonjour");
-		mail.setText("Example");
-
-		try {
-			javaMailSender.send(mail);
-		} catch (MailException e) {
-
-		}
-
+		System.out.println("Hello world");
+		sendMail.sendNotification();
 
 	}
 
