c8a27c70cf84ac66d604d90f8ce46e64f12424d1
tomneozed <tomneozed@gmail.com>
2018-11-09 21:10:04 +0100

    Created a generic login action logic

--- Application/WebApp/src/main/java/com/action/LoginAction.java
+++ Application/WebApp/src/main/java/com/action/LoginAction.java
@@ -1,18 +1,52 @@
 package com.action;
 
+import com.model.User;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import com.service.LoginService;
 import generated.bibliothekservice.BibliothekService;
 import generated.bibliothekservice.UtilisateurResponse;
+import org.apache.commons.lang3.StringUtils;
 
-public class LoginAction extends ActionSupport {
-    BibliothekService bibliothekService;
+public class LoginAction extends ActionSupport implements ModelDriven<User> {
+
+    private User user = new User();
+
+    public void validate()
+    {
+        if(StringUtils.isEmpty(user.getUsername()))
+        {
+            addFieldError("username", "Username cannot be blank");
+        }
+        if(StringUtils.isEmpty(user.getPassword()))
+        {
+            addFieldError("password", "Password cannot be blank");
+        }
+    }
 
     public String execute()
     {
-        UtilisateurResponse utilisateurResponse = bibliothekService.identification("AlainTerrieur", "MotDePasse");
 
-        utilisateurResponse.getUtilisateurBeanList();
+        LoginService loginService = new LoginService();
 
-        return "success";
+        if(loginService.verifyLogin(user))
+        {
+            return ActionSupport.SUCCESS;
+        }else
+        {
+            return ActionSupport.LOGIN;
+        }
+    }
+
+    public User getModel() {
+        return user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
